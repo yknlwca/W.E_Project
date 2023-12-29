dc877a3de6ccd76e3e5d0cc1ddfa7927aae61892
KL.Choi <145303968+yknlwca@users.noreply.github.com>
2023-12-29 21:04:15 +0900

    Delete Web directory

--- Web/myapp/tasks.py
+++
@@ -1,8 +0,0 @@
-# myapp/tasks.py
-
-from celery import shared_task
-from .views import process_and_save_video
-
-@shared_task
-def process_video_task(video_path):
-    return process_and_save_video(video_path)

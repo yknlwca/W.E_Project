84b56c64daa8cb9e6b00aed0beafb36ce0bb1722
KL.Choi <145303968+yknlwca@users.noreply.github.com>
2024-02-03 23:27:59 +0900

    reloading

--- Web/cctvproject/urls.py
+++ Web/cctvproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cctvproject project.
+URL configuration for Final_web project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,19 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# cctvproject/urls.py
-
+from django.contrib import admin
 from django.urls import path, include
-from detection import views as detection_views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', detection_views.home, name='home'),
-    path('results/<int:video_id>/', detection_views.view_results, name='view_results'),
-    path('upload/', detection_views.upload_video, name='upload'),
-
+    path('admin/', admin.site.urls),
+    path('',include('videos.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
